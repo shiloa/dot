a8e24eee923cdde258dfeb056cf404433f1b4419
Shilo Ayalon <shilo@oribi.io>
2016-09-19 09:25:26 +0300

    make config.fish opt-in for almost everything

--- fish/config.fish
+++ fish/config.fish
@@ -12,58 +12,51 @@ set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
+# You should have java installed
 set -x -g JAVA_HOME (/usr/libexec/java_home)
 
-# load rbenv
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
+# load rbenv (OPTIONAL, requires 'brew install rbenv')
+# set PATH $HOME/.rbenv/bin $PATH
+# set PATH $HOME/.rbenv/shims $PATH
+# rbenv rehash >/dev/null ^&1
+# source $HOME/.config/fish/rbenv.fish
 
-# node JS path
+# node JS path (OPTIONAL, requires 'brew install node')
 # node/npm setup
-set NODE_PATH $HOME/node_modules /usr/local/lib/node_modules
+# set NODE_PATH $HOME/node_modules /usr/local/lib/node_modules
 
-# load pyenv
+# load pyenv (OPTIONAL, requires 'brew install pyenv')
 # set -g -x PYENV_ROOT $HOME/.pyenv
 # set PATH $PYENV_ROOT/bin $PATH
 # status --is-interactive; and . (pyenv init -|psub)
 
-# export docker toolbox settings
-set -x DOCKER_TLS_VERIFY "1";
-set -x DOCKER_HOST "tcp://192.168.99.100:2376";
-set -x DOCKER_CERT_PATH "/Users/shilo/.docker/machine/machines/default";
-set -x DOCKER_MACHINE_NAME "default";
-
-# vim is the preferred editor
+# vim is the preferred editor (duh)
 set -g -x EDITOR vim
 
-set -g -x APP_ENV dev
-
-# misc apps/frameworks/languages configuration
-set -g -x GOPATH $HOME/gocode
+# misc apps/frameworks/languages configuration (OPTIONAL, requires 'brew install go')
+# set -g -x GOPATH $HOME/gocode
+# set PATH $GOPATH/bin $PATH
 
 # less coloring configuration
 set -g -x LESS '-R'
 set -g -x LESSOPEN '|~/.lessfilter %s'
 
-# add go path
-set PATH $GOPATH/bin $PATH
-
-# predefine certain aliases
+# predefine certain aliases (if you wish)
 source $HOME/.config/fish/aliases.fish
 
 # load VCS stuff
-source $HOME/.config/fish/hg.fish
 source $HOME/.config/fish/git.fish
-source $HOME/.config/fish/rbenv.fish
+# source $HOME/.config/fish/hg.fish
+
 
 # load aws keys
 # source $HOME/.config/fish/aws.fish
 
-# load some server connection definitions
-source $HOME/.config/fish/credentials.fish
+# load some server connection definitions (OPTIONAL)
+# source $HOME/.config/fish/credentials.fish
 
-source $HOME/.config/fish/fzf.fish
+# fzf (fuzzy find anything) support (OPTIONAL, 'brew install fzf')
+# source $HOME/.config/fish/fzf.fish
 
 
 function current_time
